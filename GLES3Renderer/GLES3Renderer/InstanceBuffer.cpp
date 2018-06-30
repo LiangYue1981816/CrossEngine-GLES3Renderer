8eb91b612c170f2fcbc407654c025b87682cbf20
LiangYue <LiangYue1981816@Hotmail.com>
2018-06-30 11:28:20 +0800

    + 添加InstanceBuffer

---
+++ GLES3Renderer/GLES3Renderer/InstanceBuffer.cpp
@@ -0,0 +1,124 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "InstanceBuffer.h"
+#include "VertexAttribute.h"
+
+
+static const int INSTANCE_BUFFER_SIZE = 4 * 1024;
+
+CInstanceBuffer::CInstanceBuffer(void)
+	: m_instanceFormat(0)
+	, m_instanceBuffer(0)
+	, m_instanceBufferSize(0)
+
+	, m_bDirty(false)
+{
+
+}
+
+CInstanceBuffer::~CInstanceBuffer(void)
+{
+	Destroy();
+}
+
+void CInstanceBuffer::Clear(void)
+{
+	if (m_instanceBuffer) {
+		m_bDirty = true;
+		m_instanceDatas.clear();
+	}
+}
+
+void CInstanceBuffer::SetInstance(const glm::mat4 &mtxTransform)
+{
+	if (m_instanceBuffer) {
+		m_bDirty = true;
+		m_instanceDatas.clear();
+		m_instanceDatas.push_back(mtxTransform);
+	}
+}
+
+void CInstanceBuffer::AddInstance(const glm::mat4 &mtxTransform)
+{
+	if (m_instanceBuffer) {
+		m_bDirty = true;
+		m_instanceDatas.push_back(mtxTransform);
+	}
+}
+
+void CInstanceBuffer::UpdateInstance(void)
+{
+	if (m_bDirty && m_instanceBuffer) {
+		m_bDirty = false;
+
+		GLuint size = m_instanceDatas.size() * sizeof(glm::mat4);
+
+		if (m_instanceBufferSize < size) {
+			m_instanceBufferSize = INSTANCE_BUFFER_SIZE;
+			while (m_instanceBufferSize < size) m_instanceBufferSize <<= 1;
+
+			glBindBuffer(GL_ARRAY_BUFFER, m_instanceBuffer);
+			glBufferData(GL_ARRAY_BUFFER, m_instanceBufferSize, NULL, GL_DYNAMIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, m_instanceBuffer);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, size, m_instanceDatas.data());
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+}
+
+bool CInstanceBuffer::CreateInstanceBuffer(GLuint format)
+{
+	m_instanceFormat = format;
+	m_instanceBufferSize = INSTANCE_BUFFER_SIZE;
+
+	glGenBuffers(1, &m_instanceBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_instanceBuffer);
+	glBufferData(GL_ARRAY_BUFFER, m_instanceBufferSize, NULL, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void CInstanceBuffer::Destroy(void)
+{
+	if (m_instanceBuffer) {
+		glDeleteBuffers(1, &m_instanceBuffer);
+	}
+
+	m_instanceFormat = 0;
+	m_instanceBuffer = 0;
+	m_instanceBufferSize = 0;
+}
+
+void CInstanceBuffer::SetupFormat(GLuint format)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, m_instanceBuffer);
+	{
+		GLuint instanceStride = GetInstanceStride(format);
+
+		for (GLuint indexAttribute = 0; indexAttribute < INSTANCE_ATTRIBUTE_COUNT; indexAttribute++) {
+			GLuint attribute = (1 << indexAttribute);
+
+			if (format & attribute) {
+				GLuint location = GetInstanceAttributeLocation(attribute);
+				GLuint components = GetInstanceAttributeComponents(attribute);
+				GLuint offset = GetInstanceAttributeOffset(format, attribute);
+
+				glEnableVertexAttribArray(location);
+				glVertexAttribPointer(location, components, GL_FLOAT, GL_FALSE, instanceStride, (const void *)offset);
+				glVertexAttribDivisor(location, 1);
+			}
+		}
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+GLuint CInstanceBuffer::GetInstanceCount(void) const
+{
+	return m_instanceDatas.size();
+}
+
+GLuint CInstanceBuffer::GetInstanceFormat(void) const
+{
+	return m_instanceFormat;
+}
---
+++ GLES3Renderer/GLES3Renderer/InstanceBuffer.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <vector>
+#include "glew.h"
+#include "glm/glm.hpp"
+
+
+class CInstanceBuffer
+{
+public:
+	CInstanceBuffer(void);
+	virtual ~CInstanceBuffer(void);
+
+
+public:
+	void Clear(void);
+	void SetInstance(const glm::mat4 &mtxTransform);
+	void AddInstance(const glm::mat4 &mtxTransform);
+	void UpdateInstance(void);
+
+public:
+	bool CreateInstanceBuffer(GLuint format);
+	void Destroy(void);
+
+private:
+	void SetupFormat(GLuint format);
+
+public:
+	GLuint GetInstanceCount(void) const;
+	GLuint GetInstanceFormat(void) const;
+
+
+private:
+	GLuint m_instanceFormat;
+	GLuint m_instanceBuffer;
+	GLuint m_instanceBufferSize;
+
+private:
+	bool m_bDirty;
+	std::vector<glm::mat4> m_instanceDatas;
+};
